52086909f8251cc8e58636a2fe2d6a56e58a85af
Gourav Bhowmick <gourav.bhowmick010@gmail.com>
2026-08-12 22:51:53 +0530

    Quick Access application

---
+++ src/main/java/com/example/CLIRunner/QuickAccess/QuickAccessApplication.java
@@ -0,0 +1,13 @@
+package com.example.CLIRunner.QuickAccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickAccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickAccessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CLIRunner/QuickAccess/QuickAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CLIRunner.QuickAccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickAccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
