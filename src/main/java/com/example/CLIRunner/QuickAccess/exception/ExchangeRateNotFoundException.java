b7b196bdfd461618202de9124cef09869e43a9ea
Gourav Bhowmick <gourav.bhowmick010@gmail.com>
2026-08-12 22:50:40 +0530

    Custom Exception

---
+++ src/main/java/com/example/CLIRunner/QuickAccess/exception/ExchangeRateNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.CLIRunner.QuickAccess.exception;
+
+public class ExchangeRateNotFoundException extends Exception{
+
+    public ExchangeRateNotFoundException(String message) {
+        super(message);
+    }
+}
